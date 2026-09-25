bfeb8319f28e44f081f4b66ff58a74dfcaf346b7
Krish Bikram Sah <krish@tukaatu.com>
2026-09-25 11:34:30 +0545

    Implement native app-style mobile bottom navigation dock, app drawer, and touch layout

--- src/app/(site)/SiteShell.js
+++ src/app/(site)/SiteShell.js
@@ -3,6 +3,7 @@ import { usePathname } from "next/navigation";
 import "./public-pages.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import MobileBottomNav from "./components/MobileBottomNav";
 
 export default function SiteShell({ children }) {
   const pathname = usePathname();
@@ -12,6 +13,7 @@ export default function SiteShell({ children }) {
       {!isHome && <Header />}
       {children}
       {!isHome && <Footer showCta={false} />}
+      <MobileBottomNav />
     </div>
   );
 }
